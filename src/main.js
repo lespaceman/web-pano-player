a8c6dbab4b81c7e21991a470743a5db85431e6de
Nadeem M <n.adeem@outlook.com>
2020-07-27 03:08:00 +0530

    fetch json and populate category: ongoing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Category from "./components/UI/category.js";
-import {init as initializeScene} from "./scene.js";
-
+import { init as initializeScene } from "./scene.js";
+import fetchData from "./components/API/fetchPlayerData.js";
 
 let category = new Category();
-category.handleMainCategoryClick()
+category.handleMainCategoryClick();
 
+let playerData = new fetchData("grand-hyatt");
+
+playerData
+  .drawCategoryData()
 
