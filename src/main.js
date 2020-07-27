a60a935961ce324c91a5281189c2f31f299e2adb
Nadeem M <n.adeem@outlook.com>
2020-07-28 03:08:04 +0530

    handle maincategory selection

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Category from "./components/UI/category.js";
 import { init as initializeScene } from "./scene.js";
 import DrawCategory from "./components/UI/DrawCategory.js";
 
-let category = new Category();
-category.handleMainCategoryClick();
-
 let playerData = new DrawCategory("grand-hyatt");
 
-playerData
-  .drawCategoryData()
+playerData.drawCategoryData();
+
+let category = new Category();
+category.handleMainCategorySelectionClick();
+
+document.addEventListener("DOMContentLoaded", category.handleSelection());
+
 
